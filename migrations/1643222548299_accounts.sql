394b5a7a3ac7b11a36335fb9d006770cd4556dec
olelarssen <39528957+olelarssen@users.noreply.github.com>
2022-07-26 02:36:04 +0530

    Update 1643222548299_accounts.sql

--- migrations/1643222548299_accounts.sql
+++ migrations/1643222548299_accounts.sql
@@ -13,16 +13,6 @@ CREATE TABLE IF NOT EXISTS accounts (
     updated                TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     removed                TIMESTAMP WITH TIME ZONE DEFAULT NULL
 );
-CREATE TABLE IF NOT EXISTS account_parameters (
-     id                          SERIAL PRIMARY KEY,
-     account_id                  INT NOT NULL,
-     loans_to_value_initial      NUMERIC,
-     loans_to_value_liquidation  NUMERIC,
-     leverage_ratio              NUMERIC,
-     mta                         NUMERIC,
-     mc_time                     NUMERIC,
-     CONSTRAINT fk_account_id FOREIGN KEY (account_id) REFERENCES accounts (id)
-)
+
 -- Down Migration
-DROP TABLE IF EXISTS account_parameters CASCADE;
 DROP TABLE IF EXISTS accounts;
